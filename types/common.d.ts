3834e2a77095be5b7610786058ba1fbca9aa966d
Ilan Frumer <ilanfrumer@gmail.com>
2023-08-29 13:10:24 +0300

    Initial commit

---
+++ types/common.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../engine/global.d.ts" />
+/// <reference path="../engine/IGUIObject.d.ts" />
+/// <reference path="../engine/pyrogenesis.d.ts" />
+/// <reference path="../generated/l10n.d.ts" />
+/// <reference path="../generated/globalscripts.d.ts" />
