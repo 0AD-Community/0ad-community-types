877ac13ad78ae69b490eee4634472faeea85061c
Ilan Frumer <ilanfrumer@gmail.com>
2023-08-31 11:26:39 +0300

    fixed Console.d.ts with GPT

--- engine/ps/Console.d.ts
+++ engine/ps/Console.d.ts
@@ -1,14 +1,25 @@
-// ScriptFunction::Register<&CConsole::IsActive, ConsoleGetter>(rq, "Console_GetVisibleEnabled");
-// ScriptFunction::Register<&CConsole::SetVisible, ConsoleGetter>(rq, "Console_SetVisibleEnabled");
-
 declare namespace Engine {
   /**
-   * @TODO
+   * Checks if the console is active or visible.
+   *
+   * C++: CConsole::IsActive
+   *
+   * @example
+   * const isActive = Engine.Console_GetVisibleEnabled();
+   *
+   * @returns {boolean} True if the console is active, false otherwise.
    */
-  function Console_GetVisibleEnabled(): void;
+  function Console_GetVisibleEnabled(): boolean;
 
   /**
-   * @TODO
+   * Sets the visibility of the console.
+   *
+   * C++: CConsole::SetVisible
+   *
+   * @example
+   * Engine.Console_SetVisibleEnabled(true);
+   *
+   * @param {boolean} isVisible - True to make the console visible, false to hide it.
    */
-  function Console_SetVisibleEnabled(): void;
+  function Console_SetVisibleEnabled(isVisible: boolean): void;
 }
